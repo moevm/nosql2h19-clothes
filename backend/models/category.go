b3a0d8b898c23574a150cf1865de874b14f362ab
BykovIlya <bikovbiv@gmail.com>
2019-09-21 20:17:19 +0300

    added empty functions to Category model

--- backend/models/category.go
+++ backend/models/category.go
@@ -5,3 +5,33 @@ type Category struct {
 	Name    string  `json:"name"`
 	Clothes []Cloth `json:"clothes"`
 }
+
+func CreateCategory(c Category) int64 {
+	var uid int64
+
+	return uid
+}
+
+func UpdateCategory(c Category) bool {
+	return true
+}
+
+func DeleteCategory(c Category) bool {
+	return true
+}
+
+func GetCategories() []Category {
+	var cs []Category
+
+	return cs
+}
+
+func GetCategoryById(id int64) Category {
+	var c Category
+	return c
+}
+
+func GetCategoryByName(name string) Category {
+	var c Category
+	return c
+}
