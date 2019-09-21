ec42596208b08594b16091cb3c94c637f079561a
BykovIlya <bikovbiv@gmail.com>
2019-09-21 20:29:56 +0300

    added empty functions to Cloth model

--- backend/models/cloth.go
+++ backend/models/cloth.go
@@ -7,3 +7,32 @@ type Cloth struct {
 	Notes string `json:"notes"`
 	Img   string `json:"img"`
 }
+
+func CreateCloth(c Cloth) int64 {
+	var uid int64
+
+	return uid
+}
+
+func UpdateCloth(c Cloth) bool {
+	return true
+}
+
+func DeleteCloth(c Cloth) bool {
+	return true
+}
+
+func GetClothes() []Cloth {
+	var cs []Cloth
+	return cs
+}
+
+func GetClothById(id int64) Cloth {
+	var c Cloth
+	return c
+}
+
+func GetClothesByName(name string) []Cloth {
+	var cs []Cloth
+	return cs
+}
