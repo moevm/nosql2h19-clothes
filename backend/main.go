b4dbef28dd48ef2754dd6eae5f7b60999b6e5c00
BykovIlya <bikovbiv@gmail.com>
2019-09-21 17:01:12 +0300

    added CORSMiddleware and gin

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"nosql2h19-clothes/examples/test_1/utils"
+	//"github.com/appleboy/gin-jwt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -23,4 +25,26 @@ func main() {
 
 	fmt.Println("Connected to MongoDB!")
 
+	router := gin.Default()
+	router.Use(CORSMiddleware())
+
+	router.Run(":5000")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length,X-Requested-With, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
 }
