9a00b5690add6371ab8edd87b288519d3c472533
BykovIlya <bikovbiv@gmail.com>
2019-09-21 22:24:31 +0300

    fix struct User

--- backend/models/user.go
+++ backend/models/user.go
@@ -17,6 +17,7 @@ type User struct {
 	Username string `json:"username"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
+	Days     []Day  `json:"days"`
 }
 
 func GetUserByUserName(username string) *User {
