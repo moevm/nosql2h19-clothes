e95ec00010a0381ad6f73e04ec284ffe02b73702
BykovIlya <bikovbiv@gmail.com>
2019-09-21 20:23:51 +0300

    added empty functions to Place model

--- backend/models/place.go
+++ backend/models/place.go
@@ -4,3 +4,32 @@ type Place struct {
 	Id   int64  `json:"id"`
 	Name string `json:"name"`
 }
+
+func CreatePlace(p Place) int64 {
+	var uid int64
+
+	return uid
+}
+
+func UpdatePlace(p Place) bool {
+	return true
+}
+
+func DeletePlace(p Place) bool {
+	return true
+}
+
+func GetPlaces() []Place {
+	var ps []Place
+	return ps
+}
+
+func GetPlaceById(id int64) Place {
+	var p Place
+	return p
+}
+
+func GetPlacesByName(name string) []Place {
+	var ps []Place
+	return ps
+}
