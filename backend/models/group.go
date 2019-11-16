8f59e37291fc2de6a4f371317f6580004f81307b
BykovIlya <bikovbiv@gmail.com>
2019-11-16 19:29:50 +0300

    added struct group and style

--- backend/models/group.go
+++ backend/models/group.go
@@ -1,4 +1,14 @@
 package models
 
 type Group struct {
+	Id         string `json:"id"`
+	Clothes    []Cloth
+	Date       string `json:"date"`
+	Place_name string `json:"place_name"`
+}
+
+type NewGroup struct {
+	Clothes    []Cloth
+	Date       string `json:"date"`
+	Place_name string `json:"place_name"`
 }
--- backend/models/style.go
+++ backend/models/style.go
@@ -1,1 +1,10 @@
 package models
+
+type Style struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type NewStyle struct {
+	Name string `json:"name"`
+}
