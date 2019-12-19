9ca17c4752f5b0b799dad79cd1c2deffd8b47e7c
BykovIlya <bikovbiv@gmail.com>
2019-12-20 01:48:30 +0300

    fix file name

--- backend/routes/admin.go
+++ backend/routes/admin.go
@@ -15,7 +15,7 @@ func UploadFile(c *gin.Context) {
 	}
 
 	FileName := "api/upload/" + "File_" + time.Now().Format("20060102150405") + "_" + file.Filename
-
+	fmt.Println(FileName)
 	if err := c.SaveUploadedFile(file, FileName); err != nil {
 		c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
 		return
