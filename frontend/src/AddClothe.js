c8154a1958abe4e4372c497371031cac9d956b51
spasartyom <spasartyom@gmail.com>
2019-12-20 03:38:02 +0300

    added POST for clothes

--- frontend/src/AddClothe.js
+++ frontend/src/AddClothe.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './AddSmth.css';
 import './Form.css';
+import './AddClothe.css'
 import axios from 'axios';
 import {Link} from "react-router-dom";
 
@@ -9,22 +10,34 @@ let endpoint = "http://localhost:5000";
 class AddClothe extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            name: '',
+            style: '',
+            category: ''
+        };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeCategory = this.handleChangeCategory.bind(this);
+        this.handleChangeStyle = this.handleChangeStyle.bind(this);
     }
 
-    handleChange = (event) => {
-        this.setState({value: event.target.value});
-        // console.log(event.target.value);
-        // console.log(this.state.value);
+    handleChangeName = (event) => {
+        this.setState({name: event.target.value});
+    }
+    handleChangeCategory = (event) => {
+        this.setState({category: event.target.value});
+    }
+    handleChangeStyle = (event) => {
+        this.setState({style: event.target.value});
     }
 
-    addPlace = () => {
+    addClothe = () => {
         // console.log(this.state.value);
         axios
-            .post(endpoint + "/api/home/places", {
-                name: this.state.value
+            .post(endpoint + "/api/home/clothes", {
+                name: this.state.name,
+                style: this.state.style,
+                category: this.state.category
             })
             .then((response) => {
                 alert(response.status);
@@ -33,13 +46,15 @@ class AddClothe extends Component {
     render() {
         return (
             <div className="AddPlace">
-            <h3>Форма добавления Places</h3>
-        <div className="form__container">
-            <label>Places</label><input type="text" placeholder="Place" id="place" onChange={this.handleChange}></input>
+            <h3>Форма добавления Одежды</h3>
+        <div className="form__container AddClothe">
+            <div><label>Название</label><input type="text" placeholder="Название" onChange={this.handleChangeName}></input></div>
+            <div><label>Стиль</label><input type="text" placeholder="Стиль" onChange={this.handleChangeStyle}></input></div>
+            <div><label>Категория</label><input type="text" placeholder="Категория" onChange={this.handleChangeCategory}></input></div>
         </div>
         <div className="wrap-btn">
             <button className="addSmth_button"><Link to="/home/places/">Back</Link></button>
-        <button type="submit" className="addSmth_button" onClick={this.addPlace}>Add Places</button>
+        <button type="submit" className="addSmth_button" onClick={this.addClothe}>Добавить</button>
         </div>
         </div>
     );
